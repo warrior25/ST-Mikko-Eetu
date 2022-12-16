fb0ef497061113435fa2de6936c32deeb160f8ad
Mikko Haavisto <mikko.haavisto@tuni.fi>
2022-12-16 14:43:26 +0200

    Add tests for add.js

--- test/add.test.js
+++ test/add.test.js
@@ -6,4 +6,36 @@ describe("Sum", () => {
     it("adds positive number to positive number", () =>{
         expect(add(3,6)).to.equal(9)
     });
+
+    it("adds positive number to negative number", () =>{
+        expect(add(-4,6)).to.equal(2)
+    });
+
+    it("adds neagtive number to positive number", () =>{
+        expect(add(4,-6)).to.equal(-2)
+    });
+
+    it("adds neagtive number to negative number", () =>{
+        expect(add(-4,-6)).to.equal(-10)
+    });
+
+    it("adds zero to negative number", () =>{
+        expect(add(-4,0)).to.equal(-4)
+    });
+
+    it("adds zero to positive number", () =>{
+        expect(add(4,0)).to.equal(4)
+    });
+
+    it("adds zero to zero", () =>{
+        expect(add(0,0)).to.equal(0)
+    });
+
+    it("adds negative number to zero", () =>{
+        expect(add(0,-6)).to.equal(-6)
+    });
+
+    it("adds positive number to zero", () =>{
+        expect(add(0,6)).to.equal(6)
+    });
    })
