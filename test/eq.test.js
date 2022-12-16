ea2cf2306ce2b7ef75d9847ed15ab1ee9ea8583b
Mikko Haavisto <mikko.haavisto@tuni.fi>
2022-12-16 14:58:44 +0200

    Add tests for eq.js

--- test/eq.test.js
+++ test/eq.test.js
@@ -3,7 +3,29 @@ import eq from "../src/eq.js"
 const expect = chai.expect
 
 describe("Equals", () => {
+    const object = { 'a': 1 }
+    const other = { 'a': 1 }
+
+    it("compares an object with the same object", () => {
+        expect(eq(object, object)).to.equal(true)
+    });
+
+    it("compares an object with a different object that has the same content", () => {
+        expect(eq(object, other)).to.equal(false)
+    });
+
     it("compares two equal strings", () =>{
         expect(eq("abc","abc")).to.equal(true)
     });
+
+    it("compares NaN to Nan", () => {
+        expect(eq(NaN, NaN)).to.equal(true)
+    });
+
    })
+
+describe.skip("Equals", () => {
+    it("compares a string with an object made from that string", () => {
+        expect(eq('a', Object('a'))).to.equal(false)
+    });
+})
