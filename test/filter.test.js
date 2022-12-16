6456673ec49de789316f1f028ec919108ead9a6c
Mikko Haavisto <mikko.haavisto@tuni.fi>
2022-12-16 18:09:07 +0200

    Add tests to filter.js

---
+++ test/filter.test.js
@@ -0,0 +1,15 @@
+import { expect } from "chai"
+import filter from '../src/filter.js'
+
+describe("Filter", () => {
+    it('filters objects by their attributes', () => {
+        const users = [
+            { 'user': 'barney', 'active': true },
+            { 'user': 'fred',   'active': false }
+        ]
+        expect(filter(users, ({active})=>active)).to.deep.equal([{ 'user': 'barney', 'active': true }])
+    });
+    it('filters integers by a function', () => {
+        expect(filter([1,2,3,4,5,6], (n) => {return n % 2 == 0})).deep.equal([2,4,6])
+    });
+})
