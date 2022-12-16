7f8555fd82387c9208d89068b0a5f31fbc21caa5
Mikko Haavisto <mikko.haavisto@tuni.fi>
2022-12-16 15:45:06 +0200

    Add another test for chunk.js

--- test/chunk.test.js
+++ test/chunk.test.js
@@ -6,4 +6,7 @@ describe.skip("Chunk", () => {
     it("splits an array of 4 characters to groups of 2", () =>{
         expect(chunk(['a', 'b', 'c', 'd'], 2)).to.deep.equal([['a', 'b'], ['c', 'd']])
     });
+    it("splits an array of 4 characters to groups of 3 and puts remaining elements to their own chunk", () => {
+        expect(chunk(['a', 'b', 'c', 'd'], 3)).to.deep.equal([['a','b','c'], ['d']])
+    });
    })
